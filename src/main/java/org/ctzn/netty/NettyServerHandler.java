28af6da20cdb9480c793cf4f79abe90b0b667ad0
ctznu <ctzn2010@qq.com>
2023-08-30 21:17:15 +0800

    task queue

--- src/main/java/org/ctzn/netty/NettyServerHandler.java
+++ src/main/java/org/ctzn/netty/NettyServerHandler.java
@@ -12,10 +12,26 @@ import io.netty.util.concurrent.EventExecutorGroup;
 public class NettyServerHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        System.out.println("server ctx: " + ctx);
-        ByteBuf buf = (ByteBuf) msg;
-        System.out.println("Server received: " + buf.toString(CharsetUtil.UTF_8));
-        System.out.println("Client address: " + ctx.channel().remoteAddress());
+        // 模拟很耗时的任务
+//        Thread.sleep(10 * 1000);
+//        ctx.writeAndFlush(Unpooled.copiedBuffer("Hello client! 11", CharsetUtil.UTF_8));
+        // solution 1: 用户自定义普通任务
+        ctx.channel().eventLoop().execute(new Runnable() {
+            @Override
+            public void run() {
+                    try {
+                    Thread.sleep(10 * 1000);
+                    ctx.writeAndFlush(Unpooled.copiedBuffer("Hello client! lala", CharsetUtil.UTF_8));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+//        System.out.println("server ctx: " + ctx);
+//        ByteBuf buf = (ByteBuf) msg;
+//        System.out.println("Server received: " + buf.toString(CharsetUtil.UTF_8));
+//        System.out.println("Client address: " + ctx.channel().remoteAddress());
     }
 
     @Override
